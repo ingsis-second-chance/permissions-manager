4655d1433177911c104d8bcb14453d134273fe00
martinagarciagalasso <mgarciagalasso@mail.austral.edu.ar>
2025-10-21 16:37:02 -0300

    spotless apply done

--- src/main/java/auth/AudienceValidator.java
+++ src/main/java/auth/AudienceValidator.java
@@ -7,19 +7,20 @@ import org.springframework.security.oauth2.jwt.Jwt;
 
 public class AudienceValidator implements OAuth2TokenValidator<Jwt> {
 
-    private final String audience;
+  private final String audience;
 
-    public AudienceValidator(String audience) {
-        this.audience = audience;
-    }
-    @Override
-    public OAuth2TokenValidatorResult validate(Jwt jwt) {
-        OAuth2Error error = new OAuth2Error("invalid_token", "The required audience is missing", null);
+  public AudienceValidator(String audience) {
+    this.audience = audience;
+  }
 
-        if (jwt.getAudience().contains(audience)) {
-            return OAuth2TokenValidatorResult.success();
-        } else {
-            return OAuth2TokenValidatorResult.failure(error);
-        }
+  @Override
+  public OAuth2TokenValidatorResult validate(Jwt jwt) {
+    OAuth2Error error = new OAuth2Error("invalid_token", "The required audience is missing", null);
+
+    if (jwt.getAudience().contains(audience)) {
+      return OAuth2TokenValidatorResult.success();
+    } else {
+      return OAuth2TokenValidatorResult.failure(error);
     }
+  }
 }
